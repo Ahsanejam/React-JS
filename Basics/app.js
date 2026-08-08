0487a21c6870199bb7468323f79e1f21e3e28dc2
Ahsan Ejam <ahsanejam48@gmail.com>
2026-08-08 19:32:57 +0530

    Basics before react

---
+++ Basics/app.js
@@ -0,0 +1,26 @@
+// const a = 10;
+
+// export default a
+
+
+// export const arr = [10, 20, 30, 40];
+
+
+// When you export like this you have to be careful about the name 
+// because when you import it the same name you put ther with { arr } like this
+
+// const user = "Ahsan";
+
+// export default user
+
+
+// If you use default export in the file then it only export one thing what you gave 
+// And if you import this file from another file then you gave name in import
+// Whatever you want
+
+
+const user = 'Ahsan';
+export const age = 20;
+export const skills = ["Frontend", "Backend", "Deployment"];
+
+export default user
---
+++ Basics/script.js
@@ -0,0 +1,25 @@
+// const someValue = require("./app");
+// let h1 = document.createElement('h1')
+// let body = document.querySelector("body");
+// h1.innerHTML = "Hello from JS";
+// body.appendChild(h1); 
+
+// import username from "./app.js"
+// import { arr } from './app.js';
+
+// console.log(username);
+// console.log(arr);
+
+
+// import name from "./app.js";
+// import {age, skills} from "./app.js"; 
+
+
+// console.log(age, skills);
+
+
+
+
+
+import {age} from "./app.js";
+console.log(age);
