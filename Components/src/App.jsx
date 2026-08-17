88d05f8dc52620b61be18b06bc3f87f735350b35
Ahsan Ejam <ahsanejam48@gmail.com>
2026-08-18 02:27:40 +0530

    I learn about components

---
+++ Components/src/App.jsx
@@ -0,0 +1,93 @@
+// import React from 'react'
+// import Card from './components/Card'
+
+// const App = () => {
+//   return (
+//     <div>
+//       <div className='card'>
+//         <h1>Ahsan Ejam</h1>
+//         <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit, maiores?</p>
+//       </div>
+//       {Card()}
+//     </div>
+//   )
+// }
+
+// export default App
+// In react if you want to give a class in div so you write <div classname='dad'></div>
+// you cannot give only class because class is a keywork in react but id you can give 
+// Same like html
+
+// What is components -> it is basically a piece of code which is used for one feature
+// Ek chota sa piece of code jo ko ek particular feature ke liye use ho raha hai
+
+// What's the point of give bracket to call function like previous example you see {Card()} i call like 
+// that why so you see in this program
+
+// If you want print a variable so use {} curly bracket 
+
+// import React from 'react'
+
+// const App = () => {
+//   const user = "Ahsan";
+//   const age = 89;
+//   return (
+//     <div>
+//       <h1>Hello, Guys I am {user} </h1>
+//       <h2>and I am {age} year old</h2>
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+
+// import React from 'react'
+// import Card from './components/Card'
+
+// const App = () => {
+//   // function are reuseable so we can print as much as we want 
+//   return (
+//     <div>
+      
+//       <Card />
+//       <Card />
+//       <Card />
+//       <Card />
+//       <Card />
+//       <Card />
+//       <Card />
+//       <Card />
+//       <Card />
+//       <Card />
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+// import React from 'react'
+// import Card from './components/Card'
+// import Navbar from './components/Navbar'
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Navbar />
+//       <Navbar />
+//       <Card />
+//       <Card />
+//     </div>
+//   )
+// }
+
+// export default App
+
+
+// Props drilling it means when you go to instagram page you will see that every thing in any 
+// instagram account you can clearly see the same home icon same search icon same reels position
+// but different data it means same every thing but different data 
+
+
---
+++ Components/src/components/Card.jsx
@@ -0,0 +1,24 @@
+// function Card() {
+//     return <div className='card'>
+//                 <h1>Ahsan Ejam</h1>
+//                 <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit, maiores?</p>
+//             </div>
+// }
+
+// export default Card;
+
+
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+        <div className='card'>
+            <h1>Ahsan Ejam</h1>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit, maiores?</p>
+        </div>
+    </div>
+  )
+}
+
+export default Card
